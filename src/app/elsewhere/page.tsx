b340b9c0bbc904b2bb7425833b67bdfe8e422263
Chris Hengler <chrysics@gmail.com>
2025-07-06 09:20:36 +0200

    update elsewhere page

--- src/app/elsewhere/page.tsx
+++ src/app/elsewhere/page.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from "next/image";
 import Typography from "@mui/material/Typography";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
@@ -13,45 +14,37 @@ export default function Page() {
       <List>
         <ListItem>
           <ListItemIcon>
-            <GitHubIcon htmlColor="black" />
-          </ListItemIcon>
-          <Link href="https://github.com/chrishengler">
-            @chrishengler on GitHub
-          </Link>
-        </ListItem>
-        <ListItem>
-          <ListItemIcon>
             <LinkedInIcon htmlColor="black" />
           </ListItemIcon>
           <Link href="https://www.linkedin.com/in/chris-hengler/">
             Chris Hengler on LinkedIn
           </Link>
         </ListItem>
+        <ListItem>
+          <ListItemIcon>
+            <Image
+              src="/icons/xing.png"
+              alt="Xing"
+              height={24}
+              width={24}
+              style={{ width: 24, height: 24 }}
+            />
+          </ListItemIcon>
+          <Link href="https://www.xing.com/profile/Chris_Hengler">
+            Chris Hengler on Xing
+          </Link>
+          .
+        </ListItem>
       </List>
-      <Typography variant="h6">Personal</Typography>
+
+      <Typography variant="h6">Software</Typography>
       <List>
         <ListItem>
           <ListItemIcon>
-            <img
-              src="/icons/storygraph.png"
-              alt="storygraph"
-              style={{ width: 24, height: 24 }}
-            />
+            <GitHubIcon htmlColor="black" />
           </ListItemIcon>
-          <Link href="https://app.thestorygraph.com/profile/chrishengler">
-            My StoryGraph profile
-          </Link>
-        </ListItem>
-        <ListItem>
-          <ListItemIcon>
-            <img
-              src="/icons/cornelllab.png"
-              alt="Cornell Lab of Ornithology"
-              style={{ width: 24, height: 24 }}
-            />
-          </ListItemIcon>
-          <Link href="https://ebird.org/profile/MzAyNDU5MQ">
-            My eBird profile
+          <Link href="https://github.com/chrishengler">
+            @chrishengler on GitHub
           </Link>
         </ListItem>
         <ListItem>
@@ -64,14 +57,46 @@ export default function Page() {
         </ListItem>
         <ListItem>
           <ListItemIcon>
-            <img
-              src="/icons/itch.svg"
-              alt="itch.io"
+            <Image
+              src="/icons/cunning_hound.png"
+              alt="Cunning Hound"
+              height={64}
+              width={64}
               style={{ width: 24, height: 24 }}
             />
           </ListItemIcon>
-          <Link href="https://cunninghound.itch.io/">
-            @CunningHound on itch.io
+          <Link href="https://www.cunninghound.com">Cunning Hound games</Link>
+        </ListItem>
+      </List>
+
+      <Typography variant="h6">Personal</Typography>
+      <List>
+        <ListItem>
+          <ListItemIcon>
+            <Image
+              src="/icons/storygraph.png"
+              alt="storygraph"
+              height={24}
+              width={24}
+              style={{ width: 24, height: 24 }}
+            />
+          </ListItemIcon>
+          <Link href="https://app.thestorygraph.com/profile/chrishengler">
+            My StoryGraph profile
+          </Link>
+        </ListItem>
+        <ListItem>
+          <ListItemIcon>
+            <Image
+              src="/icons/cornelllab.png"
+              alt="Cornell Lab of Ornithology"
+              height={24}
+              width={24}
+              style={{ width: 24, height: 24 }}
+            />
+          </ListItemIcon>
+          <Link href="https://ebird.org/profile/MzAyNDU5MQ">
+            My eBird profile
           </Link>
         </ListItem>
       </List>
