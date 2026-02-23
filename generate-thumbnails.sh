6ad067a91ee193418a45066cc1732dae1dcf374c
Chris Hengler <chrysics@gmail.com>
2026-02-23 15:29:05 +0100

    generate thumbails and update paths

--- generate-thumbnails.sh
+++ generate-thumbnails.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 set -euo pipefail
 
-IMAGES_DIR="$(dirname "$0")/public/images"
+IMAGES_DIR="$(dirname "$0")/public/images/"
 
-find "$IMAGES_DIR" -name "*-thumbnail.jpg" -o -name "*-thumbnail.jpeg" | xargs -r rm
-
+# Find all image files excluding thumbnails and old -1500 versions
 find "$IMAGES_DIR" \( -name "*.jpg" -o -name "*.jpeg" \) \
   ! \( -name "*-thumbnail.jpg" -o -name "*-thumbnail.jpeg" \) \
   ! \( -name "*-1500.jpg" -o -name "*-1500.jpeg" \) | \
 while read -r img; do
   ext="${img##*.}"
   thumb="${img%.$ext}-thumbnail.$ext"
-  magick "$img" -resize y100 "$thumb"
+
+  # Only generate thumbnail if it doesn't already exist
+  if [ ! -f "$thumb" ]; then
+    magick "$img" -resize x100 "$thumb"
+  fi
 done
